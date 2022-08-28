2badc90c8325423410e4e00af7419d65f59b440b
burdey <88636823+burdey@users.noreply.github.com>
2022-08-28 17:33:27 +0800

    Add files via upload

---
+++ Reverse.h
@@ -0,0 +1,18 @@
+//
+// Created by Satori on 2022/8/26.
+//
+
+#ifndef WEEK4_REVERSE_H
+#define WEEK4_REVERSE_H
+#include <conio.h>
+#include <sstream>
+#include <math.h>
+using namespace std;
+
+class Reverse {
+public:
+    string reverseString(string letter);
+    int reverseDigit(int value);
+}
+
+#endif //WEEK4_REVERSE_H
